48f2d8bfd917dbd413471ee481f2686b6aeb60e9
Gabriel Karing <gabhenkar@gmail.com>
2024-12-11 16:28:58 -0300

    New commit

---
+++ JS/index.js
@@ -0,0 +1,8 @@
+//Lista das palavras do jogo
+const listaPalavras = ["javascript","html","css","youtube","python","java"];
+
+let palavraEscolhida;
+let exibiçãoPalavra;
+let letrasChutadas;
+let tentativasRestantes;
+let numeroErros; 
