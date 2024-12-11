80389be209bccda3b18ee107a7c3b1aafacff3d8
Gabriel Karing <gabhenkar@gmail.com>
2024-12-11 16:34:56 -0300

    Update index.js

--- JS/index.js
+++ JS/index.js
@@ -6,3 +6,17 @@ let exibiçãoPalavra;
 let letrasChutadas;
 let tentativasRestantes;
 let numeroErros; 
+
+
+//Função para iniciar jogo
+function iniciarJogo(){
+    //Escolher uma palavra aleatória da lista
+    palavraEscolhida = listaPalavras[Math.floor(Math.random()*listaPalavras.length)];
+    console.log(palavraEscolhida);
+
+    //Inicializar a exibição com os UNDERSCORES
+    exibiçãoPalavra = Array(palavraEscolhida.length).fill("_");
+    console.log(exibiçãoPalavra);
+
+    window.load=iniciarJogo();
+}
